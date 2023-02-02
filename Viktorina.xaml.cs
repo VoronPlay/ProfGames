f2de6eab5f7ed00aa8cd24d597afe5b88a88f535
visual@ap47.ru <visual@ap47.ru>
2023-02-02 11:49:35 +1000

    Добавьте файлы проекта.

---
+++ Viktorina.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProfGames
+{
+    /// <summary>
+    /// Логика взаимодействия для Viktorina.xaml
+    /// </summary>
+    public partial class Viktorina : Page
+    {
+        public Viktorina()
+        {
+            InitializeComponent();
+        }
+
+        private void Nazad_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+    }
+}
